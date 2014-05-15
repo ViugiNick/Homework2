26030ca491f17e2554c4b6aef0f7d43670a079c0
ViugiNick <viuginov.nickolay@gmail.com>
2014-05-15 21:16:31 +0400

    добавил конструктор и комментарии

--- Home6/6homework3/sharedpointer.h
+++ Home6/6homework3/sharedpointer.h
@@ -1,75 +1,83 @@
 #pragma once
 
 template <typename T>
-struct Note
-{
-    T* val;
-    int cnt;
-
-    ~Note()
-    {
-        delete val;
-    }
-};
-
-template <typename T>
 class SharedPointer
 {
     public:
         SharedPointer();
         ~SharedPointer();
 
+        ///Construct from pointer
         SharedPointer(T *ptr);
-        SharedPointer(SharedPointer &ptr);
-        SharedPointer &operator= (SharedPointer &ptr);
+        ///Constructor copying
+        SharedPointer(SharedPointer const &ptr);
+        SharedPointer &operator= (SharedPointer const &ptr);
+        ///Check, if no links on this memory, than deletessssss
+        void dereference();
 
+        ///Return pointer
         T* getVal();
+        ///Number of links on this pointer
         int getCnt();
 
     private:
-        Note<T>* main;
+        struct Note
+        {
+            T* val;
+            int cnt;
+
+            ~Note()
+            {
+                delete val;
+            }
+            ///Empty constructor
+            Note():val(nullptr), cnt(0) {}
+            ///Constructor
+            Note(T* val, int cnt):val(val), cnt(cnt) {}
+        };
+        Note* main;
 };
 
 template<typename T>
 SharedPointer<T>::SharedPointer()
 {
-    main = new Note<T>;
-    main->val = nullptr;
-    main->cnt = 0;
+    main = new Note();
 }
 template <typename T>
 SharedPointer<T>::~SharedPointer()
 {
     main->cnt--;
 
-    if (main->cnt <= 0)
-        delete main;
+    dereference();
 }
-
 
 
 template<typename T>
 SharedPointer<T>::SharedPointer(T *ptr)
 {
-    main = new Note<T>;
-    main->val = ptr;
-    main->cnt = 1;
+    main = new Note(ptr, 1);
 }
 
 template<typename T>
-SharedPointer<T>::SharedPointer(SharedPointer &ptr)
+SharedPointer<T>::SharedPointer(SharedPointer const &ptr)
 {
     main = ptr.main;
     main->cnt++;
 }
 
+template<typename T>
+void SharedPointer<T>::dereference()
+{
+    if (main->cnt <= 0)
+        delete main;
+}
+
 template <typename T>
-SharedPointer<T>& SharedPointer<T>::operator= (SharedPointer &ptr)
+SharedPointer<T>& SharedPointer<T>::operator= (SharedPointer const &ptr)
 {
     main->cnt--;
 
-    if (main->cnt == 0)
-        delete main;
+    dereference();
 
     main = ptr.main;
     main->cnt++;
