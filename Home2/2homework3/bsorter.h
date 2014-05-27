b8ec59932832b74946fce87bcb2ff643d06ebbb1
ViugiNick <viuginov.nickolay@gmail.com>
2014-05-27 07:45:16 +0400

    перекинул из Homework#2

---
+++ Home2/2homework3/bsorter.h
@@ -0,0 +1,10 @@
+#include "sorter.h"
+#pragma once
+
+/// Class, for sorting array with bubble sort
+class BSorter : public Sorter
+{
+    public:
+        /// Procedure, for sorting array with bubble sort
+        void sort(int ** inputArray, int size);
+};
---
+++ Home2/2homework3/sorter.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/// Interface of class, for sorting array
+class Sorter
+{
+    public:
+        /// Procedure, for sorting array
+        virtual void sort(int ** inputArray, int size) = 0;
+};
