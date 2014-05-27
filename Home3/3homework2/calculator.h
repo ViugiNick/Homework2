671912b254dbac998b2800066af6f04df58cc6be
ViugiNick <viuginov.nickolay@gmail.com>
2014-05-27 07:47:36 +0400

    перекинул из Homework3

---
+++ Home3/3homework2/calculator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/// Class, for counting result of operation (inputNuber1 (operation) inputNuber2)
+class Calculator
+{
+    public:
+        /// Function for counting result of operation (inputNuber1 (operation) inputNuber2)
+	double calculate(double inputNumber1, double inputNuber2, char operation);
+};
