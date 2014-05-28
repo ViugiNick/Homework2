67836388450597feb139053bc0f55797790cdfbe
ViugiNick <viuginov.nickolay@gmail.com>
2014-05-28 12:04:15 +0400

    перенёс из Homework2 в Home2 папку 2-3

--- Homework#2/2-3/sorter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/// Interface of class, for sorting array
-class Sorter
-{
-    public:
-        /// Procedure, for sorting array
-        virtual void sort(int ** inputArray, int size) = 0;
-};
